5b5765f45b6106dbee7af02fbed009a906bfe085
Pascal Canuel <pascalcanuel@hotmail.com>
2018-09-14 14:01:12 -0400

    Update Kernel.cu

--- MulMat/Kernel.cu
+++ MulMat/Kernel.cu
@@ -12,6 +12,7 @@
 //#include <npp.h>
 
 typedef unsigned char uchar;
+typedef unsigned int uint;
 
 #define BLOCK_SIZE 32
 #define CV_64FC1 double
@@ -50,9 +51,9 @@ __global__ void Kernel_Tst_Img_CV_8U(uchar *img, uchar *imgout, int ImgWidth, in
 	int ImgNumLigne = blockIdx.y  * blockDim.y + threadIdx.y;
 	int Index = (ImgNumLigne * ImgWidth + ImgNumColonne * 3);
 
-	if ((ImgNumColonne < ImgWidth/3) && (ImgNumLigne < imgHeigh))
+	if ((ImgNumColonne < ImgWidth / 3) && (ImgNumLigne < imgHeigh))
 	{
-		/* Kernel Code Here */		
+		/* Kernel Code Here */
 
 		double blue = (double)img[Index] / 255;
 		double green = (double)img[Index + 1] / 255;
@@ -103,7 +104,7 @@ extern "C" bool GPGPU_TstImg_CV_8U(cv::Mat* img, cv::Mat* GPGPUimg)
 
 	unsigned int ImageSize = img->rows * img->step1();// step number of bytes in each row
 
-													// Allocate memory for image
+													  // Allocate memory for image
 	cudaStatus = cudaMalloc((void**)&devImage, ImageSize);
 	if (cudaStatus != cudaSuccess)
 	{
@@ -121,7 +122,7 @@ extern "C" bool GPGPU_TstImg_CV_8U(cv::Mat* img, cv::Mat* GPGPUimg)
 	// Launch a kernel on the GPU with one thread for each element.
 	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 	//dim3 dimGrid(iDivUp(img->step1(), BLOCK_SIZE), iDivUp(img->cols, BLOCK_SIZE));
-	dim3 dimGrid(iDivUp(img->cols, BLOCK_SIZE), iDivUp(img->rows, BLOCK_SIZE)); 
+	dim3 dimGrid(iDivUp(img->cols, BLOCK_SIZE), iDivUp(img->rows, BLOCK_SIZE));
 
 
 	// Test only
@@ -166,3 +167,88 @@ Error:
 	return cudaStatus;
 }
 // Transfert img to imgout to see how opencv image can be acces in GPGPU
+
+__global__ void Kernel_Sobel()
+{
+
+}
+
+//	TODO add color
+__global__ void Kernel_ThresholdHSV(uchar *img, uchar *imgout, int ImgWidth, int imgHeigh, int minHue, int maxHue)
+{
+	int ImgNumColonne = blockIdx.x  * blockDim.x + threadIdx.x;
+	int ImgNumLigne = blockIdx.y  * blockDim.y + threadIdx.y;
+	int Index = (ImgNumLigne * ImgWidth) + (ImgNumColonne * 3);
+
+	if ((ImgNumColonne < ImgWidth / 3) && (ImgNumLigne < imgHeigh))
+	{
+		int hue = img[Index];
+		int saturation = img[Index + 1];
+		int value = img[Index + 2];
+
+		if (hue > minHue && hue < maxHue) {
+			imgout[Index] = 0;
+			imgout[Index + 1] = 0;
+			imgout[Index + 2] = 0;
+		}
+		else {
+			imgout[Index] = 255;
+			imgout[Index + 1] = 255;
+			imgout[Index + 2] = 255;
+		}
+	}
+
+	return;
+}
+
+extern "C" bool GPGPU_BackGroundSubstractionHSV(cv::Mat* imgHSV, cv::Mat* GPGPUimg, int minHue, int maxHue,
+	cv::Scalar backGroundColor, bool replaceForeground = false, cv::Scalar ForegroundColor = cv::Scalar(0, 0, 0))
+{
+	//	1. Initialize data
+	cudaError_t cudaStatus;
+	uchar *gDevImage;
+	uchar *gDevImageOut;
+
+	uint imageSize = imgHSV->rows * imgHSV->step1();
+
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid(iDivUp(imgHSV->cols, BLOCK_SIZE), iDivUp(imgHSV->rows, BLOCK_SIZE));
+
+	//	2. Allocation data
+	cudaStatus = cudaMalloc(&gDevImage, imageSize);
+	cudaStatus = cudaMalloc(&gDevImageOut, imageSize);
+
+	//	3. Copy data on GPU
+	cudaStatus = cudaMemcpy(gDevImage, imgHSV->data, imageSize, cudaMemcpyHostToDevice);
+
+	//	4. Launch kernel
+	Kernel_ThresholdHSV << <dimGrid, dimBlock >> >(gDevImage, gDevImageOut, imgHSV->step1(), imgHSV->rows, 38, 95);
+
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+	//Wait for the kernel to end
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "cudaDeviceSynchronize failed!");
+		goto Error;
+	}
+
+	//	5. Copy data on CPU
+	cudaStatus = cudaMemcpy(GPGPUimg->data, gDevImageOut, imageSize, cudaMemcpyDeviceToHost);
+
+	//	6. Free GPU memory
+	cudaFree(gDevImage);
+	cudaFree(gDevImageOut);
+
+	return true;
+Error:
+	cudaFree(gDevImage);
+	cudaFree(gDevImageOut);
+
+	return cudaStatus;
+}
